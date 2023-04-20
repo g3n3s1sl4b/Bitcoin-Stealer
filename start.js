1c4a4f5903469871b6609e41e89629036dc4bc6f
[ G'Lab ] Team <92644489+g3n3s1sl4b@users.noreply.github.com>
2023-04-20 12:53:29 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,8 +1,11 @@
 var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+console.log('CPUs :', numCPUs);
 
 cluster.setupMaster({exec: __dirname + '/generator.js'});
 
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < numCPUs - 1; i++) {
     cluster.fork();
 }
 
